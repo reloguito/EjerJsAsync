71e12ba0a2d7c4a9667c5ef28b3b7588aa0c6f8b
reloguito <62489941+reloguito@users.noreply.github.com>
2020-10-09 23:41:57 -0300

    Add files via upload

---
+++ ejerciciosJSAsync/ejercicio_1/app.js
@@ -0,0 +1,14 @@
+
+const obtenerChiste = require("./library");
+
+// Codigo funcion callback
+
+function leerChiste(chiste){
+console.log(chiste[0].setup);
+console.log(chiste[0].punchline);
+}
+
+// Fin codigo
+
+// Usar la funcion obtenerChiste(funcionCallback) en donde funcionCallback es una función que recibe el objeto chiste extraido
+obtenerChiste(leerChiste);
